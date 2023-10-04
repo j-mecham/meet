ad15524c2fd20bc82c4bed7731701ffc4fee8d40
j-mecham <josh.mecham@gmail.com>
2023-10-04 15:01:51 -0600

    fixed problems flagged by eslint

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,34 +1,36 @@
 // src/__tests__/EventList.test.js
 
-import { render, within, waitFor } from '@testing-library/react';
+import { render, within, waitFor, screen } from '@testing-library/react';
 import { getEvents } from '../api';
 import EventList from '../components/EventList';
 import App from "../App";
 
 describe('<EventList /> component', () => {
- let EventListComponent;
- beforeEach(() => {
-   EventListComponent = render(<EventList />);
- })
+  const setup = (events=[]) => {
+    render(<EventList events={events} />);
+  }
 
  test('has an element with "list" role', () => {
-   expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
+  setup();
+   expect(screen.getByRole("list")).toBeInTheDocument();
  });
 
  test('renders correct number of events', async () => {
   const allEvents = await getEvents(); 
-  EventListComponent.rerender(<EventList events={allEvents} />);
-expect(EventListComponent.getAllByRole("listitem")).toHaveLength(allEvents.length);
+  setup(allEvents)
+  expect(screen.getAllByRole("listitem")).toHaveLength(allEvents.length);
 });
 });
 
 describe('<EventList /> integration', () => {
   test('renders a list of 32 events when the app is mounted and rendered', async () => {
-    const AppComponent = render(<App />);
-    const AppDOM = AppComponent.container.firstChild;
-    const EventListDOM = AppDOM.querySelector('#event-list');
+    // const AppComponent = render(<App />);
+    // const AppDOM = AppComponent.container.firstChild;
+    // const EventListDOM = screen.queryByTestId("event-list");
+    render(<App />);
     await waitFor(() => {
-      const EventListItems = within(EventListDOM).queryAllByRole('listitem');
+      const EventListDOM = screen.getByTestId("event-list");
+      const EventListItems = within(EventListDOM).getAllByRole("listitem");
       expect(EventListItems.length).toBe(32);
     });
   });
