d03190228acc3b4157b36c7fa061561a02aaed78
j-mecham <josh.mecham@gmail.com>
2023-10-09 17:03:38 -0600

    used OOP to implement info and error alerts

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
 import { useEffect, useState } from 'react';
 import { extractLocations, getEvents } from './api';
+import { InfoAlert, ErrorAlert } from './components/Alert';
 
 import './App.css';
 
@@ -13,6 +14,8 @@ const App = () => {
   const [currentNOE, setCurrentNOE] = useState(32);
   const [events, setEvents] = useState([]);
   const [currentCity, setCurrentCity] = useState("See all cities");
+  const [infoAlert, setInfoAlert] = useState("");
+  const [errorAlert, setErrorAlert] = useState("");
 
   const fetchData = async () => {
     const allEvents = await getEvents();
@@ -29,9 +32,20 @@ const App = () => {
 
   return (
     <div className="App">
+      <div className='alerts-container'>
+        {infoAlert.length ? <InfoAlert text={infoAlert}/> : null}
+        {errorAlert.length ? <ErrorAlert text={errorAlert}/> : null}
+      </div>
       <h1>Find an Event</h1>
-      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
-      <NumberOfEvents setCurrentNOE={setCurrentNOE} />
+      <CitySearch 
+      allLocations={allLocations} 
+      setCurrentCity={setCurrentCity}
+      setInfoAlert={setInfoAlert} 
+      />
+      <NumberOfEvents 
+      setCurrentNOE={setCurrentNOE} 
+      setErrorAlert={setErrorAlert}
+      />
       <EventList events={events}/>
     </div>
   );
--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,7 +1,30 @@
-const NumberOfEvents = ({ setCurrentNOE }) => {
+// src/components/NumberOfEvents.js
+
+import { useState, useEffect } from "react";
+
+const NumberOfEvents = ({ setCurrentNOE, setErrorAlert }) => {
   const handleInputChanged = (event) => {
     const value = event.target.value;
-    setCurrentNOE(value);
+
+    let errorText;
+    if (isNaN(value) || value <= 0 || value > 50 ) {
+      errorText = "Please make sure you input a number between 1 and 50"
+    } else {
+      errorText = ""
+      setCurrentNOE(value);
+    }
+
+    setErrorAlert(errorText);
+
+    // setSuggestions(filteredLocations);
+
+    // let infoText;
+    // if (filteredLocations.length === 0) {
+    //   infoText = "We can not find the city you are looking for. Please try another city"
+    // } else {
+    //   infoText = ""
+    // }
+    // setInfoAlert(infoText);
   };
   
     return (
