8210655cda075f253f14ce01931e915d687e610d
j-mecham <josh.mecham@gmail.com>
2023-10-16 11:35:12 -0600

    additional scatterchart edits

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -13,18 +13,18 @@ import {
 const CityEventsChart = ({ allLocations, events }) => {
     const [data, setData] = useState({});
 
-    useEffect(() => {
-        setData(getData());
-    }, [`${events}`]);
-
     const getData = () => {
         const data = allLocations.map((location) => {
         const count = events.filter((event) => event.location === location).length
-          const city = location.split(', ')[0]
+          const city = location.split((/, | - /))[0]
           return { city, count };
         })
         return data;
       };
+
+    useEffect(() => {
+      setData(getData());
+    }, [`${events}`]);
 
     return (
         <ResponsiveContainer width="99%" height={400}>
@@ -32,7 +32,7 @@ const CityEventsChart = ({ allLocations, events }) => {
             margin={{
               top: 20,
               right: 20,
-              bottom: 20,
+              bottom: 60,
               left: 20,
             }}
           >
@@ -43,7 +43,7 @@ const CityEventsChart = ({ allLocations, events }) => {
             />
             <YAxis type="number" dataKey="count" name="Number of Events" allowDecimals={false} />
             <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-            <Scatter name="A school" data={data} fill="#8884d8" />
+            <Scatter name="Events" data={data} fill="#8884d8" />
           </ScatterChart>
         </ResponsiveContainer>
     );
